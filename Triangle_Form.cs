7e1b9152cea8bde2868cf80108ab449e45da9830
opilane <opilane@HP-CZC2349HTB>
2024-11-04 15:47:56 +0200

    Add project files.

---
+++ Triangle_Form.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tringle
+{
+    public partial class TriangleForm : Form
+    {
+        private Triangle triangle;
+
+        // Конструктор формы, принимающий объект треугольника
+        public TriangleForm(Triangle triangle)
+        {
+            
+            this.triangle = triangle;
+            DisplayTriangleInfo();
+        }
+
+        private void DisplayTriangleInfo()
+        {
+            // Метки для отображения информации о треугольнике
+            Label labelA = new Label { Text = "Side A: " + triangle.OutputA(), Location = new Point(10, 10) };
+            Label labelB = new Label { Text = "Side B: " + triangle.OutputB(), Location = new Point(10, 40) };
+            Label labelC = new Label { Text = "Side C: " + triangle.OutputC(), Location = new Point(10, 70) };
+            Label labelPerimeter = new Label { Text = "Perimeter: " + triangle.Perimeter(), Location = new Point(10, 100) };
+            Label labelSurface = new Label { Text = "Surface: " + triangle.Surface(), Location = new Point(10, 130) };
+            Label labelExist = new Label
+            {
+                Text = "Triangle Exists: " + (triangle.ExistTriangle ? "Yes" : "No"),
+                Location = new Point(10, 160)
+            };
+
+            // Добавляем метки на форму
+            Controls.Add(labelA);
+            Controls.Add(labelB);
+            Controls.Add(labelC);
+            Controls.Add(labelPerimeter);
+            Controls.Add(labelSurface);
+            Controls.Add(labelExist);
+        }
+    }
+}
---
+++ Tringle_Vorm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tringle
+{
+    public partial class Tringle_Vorm : Form
+    {
+        public Tringle_Vorm()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Создаем кнопку Start
+            Button startButton = new Button();
+            startButton.Text = "Start";
+            startButton.Font = new Font("Arial", 28, FontStyle.Regular);
+            startButton.Size = new Size(150, 80);
+            startButton.BackColor = Color.Pink;
+            startButton.ForeColor = Color.White;
+            startButton.FlatStyle = FlatStyle.Flat;
+            startButton.FlatAppearance.BorderColor = Color.Black;
+            startButton.Location = new Point(50, 50);
+
+            // Добавляем обработчик события для кнопки
+            startButton.Click += StartButton_Click;
+
+            // Добавляем кнопку на форму
+            Controls.Add(startButton);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // Создаем экземпляр треугольника с заданными сторонами
+            Triangle triangle = new Triangle(3, 4, 5); // Пример значений для сторон a, b и c
+
+            // Открываем форму TriangleForm и передаем туда объект треугольника
+            TriangleForm triangleForm = new TriangleForm(triangle);
+            triangleForm.Show();
+        }
+    }
+}
